4ff0fef8781647960ae3b0abcfc87ef3c2bcbf11
Darren McBain (OboShape) <dar_mcbain@yahoo.co.uk>
2019-05-18 18:47:45 +0100

    after implementing Click to move and only on horizontal and vertical tiles.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public int borderPadding = 0;
+
+    public GameObject tilePrefab;
+    public GameObject[] gamePiecePrefabs;
+
+    // empty container objects
+    GameObject m_tileContainer;
+    GameObject m_gamePieceContainer;
+
+    Tile [,] m_allTiles;
+    GamePiece[,] m_allGamePieces;
+
+    Tile m_clickedTile;
+    Tile m_targetTile;
+
+    public float moveDuration = 0.2f;
+   
+ 
+    void Start()
+    {
+        // create a couple of empties to be parent holders for Tiles and GamePiesces
+        m_tileContainer = new GameObject("Tile Container");
+        m_gamePieceContainer = new GameObject("GamPiece Container");
+
+        m_tileContainer.transform.SetParent(this.transform);
+        m_gamePieceContainer.transform.SetParent(this.transform);
+
+
+        m_allTiles = new Tile[width, height];
+        SetupTiles();
+
+        m_allGamePieces = new GamePiece[width, height];
+        SetupCamera();
+
+        FillRandom();
+    }
+
+    void SetupTiles()
+    {
+        for (int i = 0; i < width; i++ )
+        {
+            for ( int j = 0; j < height; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0.0f), Quaternion.identity, m_tileContainer.transform) as GameObject;
+                tile.name = "Tile ( " + i + "," + j + " )";
+                m_allTiles[i, j] = tile.GetComponent<Tile>();
+                m_allTiles[i, j].Init(i, j, this);
+            }
+        }
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((float)(width-1) / 2f, (float)(height-1) / 2f, -10f);
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+
+        float cameraSizeVertical = height / 2.0f + borderPadding;
+        float cameraSizeHorizontal = (((float)width / 2f + (float)borderPadding) / aspectRatio);
+
+        Camera.main.orthographicSize = (cameraSizeHorizontal > cameraSizeVertical) ? cameraSizeHorizontal : cameraSizeVertical;
+    }
+
+    public GameObject GetRandomGamePiece()
+    {
+        int randomIndex = Random.Range(0, gamePiecePrefabs.Length);
+        if (gamePiecePrefabs[randomIndex] == null)
+        {
+            Debug.LogWarning("BOARD : " + randomIndex + " does not contain a valid gamepiece prefab!");
+        }
+        return gamePiecePrefabs[randomIndex];
+    }
+
+    public void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        if (gamePiece == null)
+        {
+            Debug.LogWarning("BOARD : Invalid GamePiece!");
+        }
+
+        gamePiece.transform.position = new Vector3(x, y, 0f);
+        gamePiece.transform.rotation = Quaternion.identity;
+        if (IsWithinBounds(x, y))
+        {
+            m_allGamePieces[x, y] = gamePiece;
+        }
+        gamePiece.SetCoord(x, y);
+    }
+
+    bool IsWithinBounds (int x, int y)
+    {
+        return (x >= 0 && x < width && y >= 0 && y < height);
+    }
+
+    void FillRandom()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity, m_gamePieceContainer.transform) as GameObject;
+                if (randomPiece != null)
+                {
+                    PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+                    randomPiece.GetComponent<GamePiece>().Init(this, moveDuration);
+                }
+            }
+        }
+    }
+
+    public void CLickTile(Tile tile)
+    {
+        if (m_clickedTile == null)
+        {
+            m_clickedTile = tile;
+            //Debug.Log("Clicked Tile : " + tile.name);
+        }
+    }
+
+    public void DragToTile(Tile tile)
+    {
+        if (m_clickedTile != null && IsNextTo(m_clickedTile, tile))
+            m_targetTile = tile;
+    }
+
+    public void ReleaseTile()
+    {
+        if (m_clickedTile != null && m_targetTile != null)
+        {
+            SwitchTiles(m_clickedTile, m_targetTile);
+        }
+        m_clickedTile = null;
+        m_targetTile = null;
+    }
+
+    void SwitchTiles(Tile clickedTile, Tile targetTile)
+    {
+        GamePiece clickedPiece = m_allGamePieces[clickedTile.xIndex, clickedTile.yIndex];
+        GamePiece targetPiece = m_allGamePieces[targetTile.xIndex, targetTile.yIndex];
+
+        clickedPiece.Move(targetTile.xIndex, targetTile.yIndex);
+        targetPiece.Move(clickedTile.xIndex, clickedTile.yIndex);
+    }
+
+    bool IsNextTo(Tile start, Tile end)
+    {
+        if (Mathf.Abs(start.xIndex - end.xIndex) == 1 && start.yIndex == end.yIndex)
+        {
+            return true;
+        }
+        if (Mathf.Abs(start.yIndex - end.yIndex) == 1 && start.xIndex == end.xIndex)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/GamePiece.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+
+    Board m_board;
+
+    bool m_isMoving = false;
+    float m_moveDuration = .2f;
+
+    public InterpType interpolation = InterpType.SmootherStep;
+
+    public enum InterpType
+    {
+        Linear,
+        EaseOut,
+        EaseIn,
+        SmoothStep,
+        SmootherStep
+    };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    Move((int)transform.position.x + 1, (int)transform.position.y, .5f);
+        //}
+        //if (Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    Move((int)transform.position.x - 1, (int)transform.position.y, .5f);
+        //}
+    }
+
+    public void Init(Board board, float _duration)
+    {
+        m_board = board;
+        m_moveDuration = _duration;
+    }
+
+    public void SetCoord(int x, int y)
+    {
+        xIndex = x;
+        yIndex = y;
+    }
+
+    public void Move( int destX, int destY)
+    {
+        if(!m_isMoving)
+            StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), m_moveDuration));
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        Vector3 startPosition = transform.position;
+
+        bool reachedDestination = false;
+        float elapsedTime = 0f;
+
+        m_isMoving = true;
+
+        while (!reachedDestination)
+        {
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                reachedDestination = true;
+
+                if (m_board != null)
+                    m_board.PlaceGamePiece(this, (int)destination.x, (int)destination.y);
+                break;
+            }
+
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / timeToMove;
+
+            switch (interpolation)
+            {
+                case InterpType.Linear:
+                    break;
+                case InterpType.EaseOut:
+                    t = Mathf.Sin(t * Mathf.PI * 0.5f);
+                    break;
+                case InterpType.EaseIn:
+                    t = 1 - Mathf.Cos(t * Mathf.PI + 0.5f);
+                    break;
+                case InterpType.SmoothStep:
+                    t = t * t * (3 - 2 * t);
+                    break;
+                case InterpType.SmootherStep:
+                    t = t * t * t * (t * (t * 6 - 15) + 10);
+                    break;
+            }
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+            yield return null;
+        }
+
+        m_isMoving = false;
+    }
+}
